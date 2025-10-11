cb2a0751d3da8f0dc85e256b3752af3c08f77ac2
husain <husain68644@gmail.com>
2025-10-11 10:13:37 +0530

    .

--- script.sh
+++ script.sh
@@ -3,10 +3,13 @@
 
 git init
 git add .
-git commit -m "Initial commit"
-git branch -M master
-git remote add origin "git@github.com:Husain644/ReactJs_Vite.git"
-git push -u origin master
+git commit -m "."
+git push
+
+# git branch -M master
+
+
+# git remote add origin "git@github.com:Husain644/ReactJs_Vite.git"
+# git push -u origin master
   
 
-# Merged feature-branch into main   
